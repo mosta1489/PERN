b761affe549567d63e266a480fdd80b90df67872
mosta1489 <mosta148999@gmail.com>
2022-09-27 02:49:02 +0200

    create react app and buld structure of app

---
+++ todo/client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+
+// comoponents
+
+import InputTodo from "./components/InputTodo";
+
+const App = () => {
+  return (
+    <Fragment>
+      <div className="container">
+        <InputTodo />
+      </div>
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ todo/client/src/components/InputTodo.jsx
@@ -0,0 +1,40 @@
+import React, { Fragment, useState } from "react";
+const InputTodo = () => {
+  const [description, setDescription] = useState("");
+  const changeDescription = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    const body = { description };
+    try {
+      const response = await fetch("http://localhost:3000/todo", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1 className="text-center mt-5">Pern Todo List</h1>
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          className="form-control"
+          value={description}
+          onChange={changeDescription}
+        />
+        <button className="btn btn-success">Add</button>
+      </form>
+      <h2>{description}</h2>
+    </Fragment>
+  );
+};
+
+export default InputTodo;
