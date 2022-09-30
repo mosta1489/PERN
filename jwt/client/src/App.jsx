1abb0b91400430b22eb92a604f2f6e2de9dd4e4f
mosta1489 <mosta148999@gmail.com>
2022-09-30 19:18:45 +0200

    build routers of app

--- jwt/client/src/App.jsx
+++ jwt/client/src/App.jsx
@@ -1,7 +1,27 @@
-import React, { useState, useRef } from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// Components
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+
+const login = () => {
+  return <h1>Login</h1>;
+};
 
 const App = () => {
-  return;
+  return (
+    <div className="container">
+      <Router>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ jwt/client/src/App2.jsx
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Redirect,
+// } from "react-router-dom";
+
+// // Components
+// import Dashboard from "./components/Dashboard";
+// import Login from "./components/Login";
+// import Register from "./components/Register";
+
+const App2 = () => {
+  return (
+    // <Fragment>
+    <h1>router</h1>
+    // </Fragment>
+  );
+};
+
+export default App2;
